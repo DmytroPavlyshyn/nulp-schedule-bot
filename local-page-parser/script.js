21d4daf182fb56eb2a70827836cd1aa3df643280
ilunka00 <ilunka00@gmail.com>
2018-05-10 19:47:54 +0300

    Added array of groups

--- local-page-parser/script.js
+++ local-page-parser/script.js
@@ -1,11 +1,22 @@
 const inst = []
+const groups = []
 
 $('#stud select[name="inst"]').find('option').each((index, option) => {
     const $option = $(option)
     inst.push({
         value: $option.attr('value'),
         name: $option.text()
-    })
+    })  
 });
 
 console.log(inst)
+
+$('#stud select[name="group"]').find('option').each((index, option) => {
+    const $option = $(option)
+    groups.push({
+        value: $option.attr('value'),
+        name: $option.text()
+    })
+});
+
+console.log(groups);
