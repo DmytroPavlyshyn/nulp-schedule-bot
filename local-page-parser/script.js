041f13c40e363253645be0fa5e8e3deab9a06400
ilunka00 <ilunka00@gmail.com>
2018-05-10 22:38:48 +0300

    Made more productive x2

--- local-page-parser/script.js
+++ local-page-parser/script.js
@@ -16,11 +16,22 @@ console.log(inst)
 
 $('#stud select[name="group"]').find('option').each((index, option) => {
     const $option = $(option)
+    const value=$option.attr('value');
     groups.push({
-        value: $option.attr('value'),
+        value,
         name: $option.text()
     })
 });
 
 console.log(groups);
 
+$('#stud select[name="inst"]').find('option').each((index, option) => {
+    const $option = $(option)
+    const value=$option.attr('value');
+    if (value){
+        inst.push({
+            value,
+            name: $option.text()
+        })  
+    }
+});
